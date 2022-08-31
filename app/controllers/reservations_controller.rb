a05c16dc44b8e4a2fc3dd87dfe5ebfabd8040aa8
堀田太陽 <hottataiyo@hottataiyounoMacBook-Air.local>
2022-08-31 21:31:31 +0900

    前回ログイン前の情報保持のためsessionを実装したが、削除のタイミングによりブラウザの バックとフォワードに対応できていなかったため、sessionを保持しつつ、paramsが代入されていない場合のみ反映し、言い換えるとparamsが代入されている場合は、paramsを用いるよう実装した。なお削除のタイミングは、reservationコントローラーのnewではなく、createアクション実行時としている。

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -10,6 +10,7 @@ class ReservationsController < ApplicationController
     
     if @reservation.save
       flash[:notice] = "予約を完了しました"
+      session.delete("form_data")
       redirect_to reservation_path(@reservation.id)
     else
       flash[:error_notice] = "予約ができませんでした"
@@ -28,127 +29,86 @@ class ReservationsController < ApplicationController
     redirect_to reservations_path
   end
 
-  # def new
-  #   @reservation = Reservation.new
-
-  #   if params[:day_start].blank? || params[:day_end].blank? || params[:number].blank?
-  #     flash[:notice] = "未入力の箇所があります"
-  #     redirect_to room_path(params[:room_id]) 
-  #   else
-  #     @dayStart = params[:day_start].to_date
-  #     @dayEnd = params[:day_end].to_date 
-  #     @number = params[:number].to_i
-
-  #     if @number <= 0
-  #       flash[:notice] = "人数の入力値に誤りがあります"
-  #       redirect_to room_path(params[:room_id]) 
-  #     end
-      
-  #     if @dayEnd.after? @dayStart
-  #       if @dayStart.after? Date.today
-  #         @term = (@dayEnd - @dayStart).to_i 
-  #         @total = (params[:payment]).to_i * (params[:number]).to_i * (@dayEnd - @dayStart).to_i
-  #       else
-  #         flash[:notice] = "日付を見直してください"
-  #         redirect_to room_path(params[:room_id]) 
-  #       end
-  #     else
-  #       flash[:notice] = "日付を見直してください"
-  #       redirect_to room_path(params[:room_id]) 
-  #     end
-  #   end
-
-  # end
-
   def new
     if current_user.nil?
-      if params[:day_start].blank? || params[:day_end].blank? || params[:number].blank?
+      @dayStart = params[:day_start].to_date
+      @dayEnd = params[:day_end].to_date 
+      @number = params[:number].to_i
+
+      if @number <= 0
+        flash[:notice] = "人数の入力値に誤りがあります"
+        redirect_to room_path(params[:room_id]) 
+      end
+
+      if @dayStart.blank? || @dayEnd.blank? || @number.blank? 
         flash[:notice] = "未入力の箇所があります"
         redirect_to room_path(params[:room_id]) 
+      end
+      
+      if (@dayStart.after? Date.today) && (@dayEnd.after? @dayStart)
+        @term = (@dayEnd - @dayStart).to_i 
+        @total = (params[:payment]).to_i * (params[:number]).to_i * (@dayEnd - @dayStart).to_i
+        
+        session[:form_data] = {}
+        session[:form_data]["day_start"] = params[:day_start]
+        session[:form_data]["day_end"] = params[:day_end]
+        session[:form_data]["number"] = params[:number]
+        session[:form_data]["payment"] = params[:payment]
+        session[:form_data]["room_id"] = params[:room_id]
+        session[:form_data]["term"] = @term
+        session[:form_data]["total"] = @total
+
+        redirect_to new_user_session_path    
       else
+        flash[:notice] = "日付を見直してください"
+        redirect_to room_path(params[:room_id]) 
+      end
+
+    else
+      @reservation = Reservation.new
+
+      # "params[:room_id].blank" の条件分岐によって、sessionを削除せずとも、
+      #  paramsが代入されていない時のみにsession情報を反映させ、
+      #  言い換えると、paramsが代入されている時は、paramsを優先させることができる。
+      # sessionを残すことで、ブラウザのバックとフォワードに対応することができた
+      # session自体は、現状createアクション時に削除する実装としているが、弊害はとりわけ見当たらない
+
+      if params[:room_id].blank?   
+        if session[:form_data].present?
+          @term = session[:form_data]["term"] 
+          @total = session[:form_data]["total"] 
+          @dayStart = session[:form_data]["day_start"]
+          @dayEnd = session[:form_data]["day_end"]
+          @number = session[:form_data]["number"].to_i
+          @payment = session[:form_data]["payment"] 
+          @room_id = session[:form_data]["room_id"]
+        end
+      else
+        @room_id = params[:room_id] 
+        @payment = params[:payment]
         @dayStart = params[:day_start].to_date
         @dayEnd = params[:day_end].to_date 
         @number = params[:number].to_i
 
-        if @number <= 0
-          flash[:notice] = "人数の入力値に誤りがあります"
-          redirect_to room_path(params[:room_id]) 
-        end
-        
-        if @dayEnd.after? @dayStart
-          if @dayStart.after? Date.today
-
-            session[:form_data] = {}
-            session[:form_data]["day_start"] = params[:day_start]
-            session[:form_data]["day_end"] = params[:day_end]
-            session[:form_data]["number"] = params[:number]
-            session[:form_data]["room_id"] = params[:room_id]
-            session[:form_data]["payment"] = params[:payment]
-
-            @term = (@dayEnd - @dayStart).to_i 
-            @total = (params[:payment]).to_i * (params[:number]).to_i * (@dayEnd - @dayStart).to_i
-            session[:form_data]["term"] = @term
-            session[:form_data]["total"] = @total
-
-            redirect_to new_user_session_path    
-
-          else
-            flash[:notice] = "日付を見直してください"
-            redirect_to room_path(params[:room_id]) 
-          end
-        else
-          flash[:notice] = "日付を見直してください"
-          redirect_to room_path(params[:room_id]) 
-        end
-      end
-
-    else
-      if session[:form_data]
-
-        @reservation = Reservation.new
-        @term = session[:form_data]["term"] 
-        @total = session[:form_data]["total"] 
-        @dayStart = session[:form_data]["day_start"].to_date
-        @dayEnd = session[:form_data]["day_end"].to_date
-        @number = session[:form_data]["number"].to_i
-        @room_id = session[:form_data]["room_id"] 
-        @payment = session[:form_data]["payment"] 
-
-      else
-        @reservation = Reservation.new
-        @room_id = params[:room_id] || session[:form_data]["room_id"] 
-        @payment = params[:payment]
-
-        session.delete(:form_data) 
-
-        if params[:day_start].blank? || params[:day_end].blank? || params[:number].blank?
+        if @dayStart.blank? || @dayEnd.blank? || @number.blank?
           flash[:notice] = "未入力の箇所があります"
           redirect_to room_path(@room_id)
-        else
-          @dayStart = params[:day_start].to_date
-          @dayEnd = params[:day_end].to_date 
-          @number = params[:number].to_i
+        end
 
-          if @number <= 0
-            flash[:notice] = "人数の入力値に誤りがあります"
-            redirect_to room_path(@room_id) 
-          end
-          
-          if @dayEnd.after? @dayStart
-            if @dayStart.after? Date.today
-              @term = (@dayEnd - @dayStart).to_i 
-              @total = (params[:payment]).to_i * (params[:number]).to_i * (@dayEnd - @dayStart).to_i
-              
-            else
-              flash[:notice] = "日付を見直してください"
-              redirect_to room_path(@room_id) 
-            end
-          else
-            flash[:notice] = "日付を見直してください"
-            redirect_to room_path(@room_id) 
-          end
+        if @number <= 0
+          flash[:notice] = "人数の入力値に誤りがあります"
+          redirect_to room_path(@room_id) 
+        end
+        
+        if (@dayStart.after? Date.today) && (@dayEnd.after? @dayStart)
+          @term = (@dayEnd - @dayStart).to_i 
+          @total = (params[:payment]).to_i * (params[:number]).to_i * (@dayEnd - @dayStart).to_i
+        else
+          flash[:notice] = "日付を見直してください"
+          redirect_to room_path(@room_id) 
         end
       end
+
     end
 
   end
