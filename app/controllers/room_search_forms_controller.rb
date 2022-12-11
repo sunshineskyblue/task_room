3f14ccb5c221bb37aaf7e27ade1c9d6a39162575
堀田太陽 <hottataiyo@hottataiyounoMacBook-Air.local>
2022-12-11 11:18:03 +0900

    検索結果の件数をページごとではなく、全体の件数に変更

--- app/controllers/room_search_forms_controller.rb
+++ app/controllers/room_search_forms_controller.rb
@@ -6,11 +6,12 @@ class RoomSearchFormsController < ApplicationController
 
     if search_form.valid?
       @search_title = search_form.link_adresses_keywords
-      @rooms = search_form.search_and_condition.
+      rooms = search_form.search_and_condition.
         with_attached_room_image.
-        order(updated_at: 'DESC').
-        page(params[:page]).
-        per(8)
+        order(updated_at: 'DESC')
+
+      @count = rooms.size
+      @rooms = rooms.page(params[:page]).per(8)
     else
       redirect_back(fallback_location: root_path)
     end
@@ -19,11 +20,12 @@ class RoomSearchFormsController < ApplicationController
   def area_search
     search_form = RoomSearchForm.new(search_params)
     @search_title = search_form.link_adresses_keywords
-    @rooms = search_form.search_or_condition.
+    rooms = search_form.search_or_condition.
       with_attached_room_image.
-      order(updated_at: 'DESC').
-      page(params[:page]).
-      per(8)
+      order(updated_at: 'DESC')
+
+    @count = rooms.size
+    @rooms = rooms.page(params[:page]).per(8)
   end
 
   private
