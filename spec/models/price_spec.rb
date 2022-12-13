796a513da4cc24dc67baca69592f970c6eae89a9
堀田太陽 <hottataiyo@hottataiyounoMacBook-Air.local>
2022-12-13 16:57:14 +0900

    Priceインスタンスの生成テストを追加
    Roomのfeeカラムへの依存を除いたため、Roomのシステム及びPriceのモデルテストで生成を保証する

--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -8,6 +8,30 @@ RSpec.describe Price, type: :model do
       price.value = 4999
       price.switch_price_range
       expect(price.range).to eq 1
+
+      price.value = 9999
+      price.switch_price_range
+      expect(price.range).to eq 2
+
+      price.value = 19999
+      price.switch_price_range
+      expect(price.range).to eq 3
+
+      price.value = 39999
+      price.switch_price_range
+      expect(price.range).to eq 4
+
+      price.value = 59999
+      price.switch_price_range
+      expect(price.range).to eq 5
+
+      price.value = 99999
+      price.switch_price_range
+      expect(price.range).to eq 6
+
+      price.value = 100000
+      price.switch_price_range
+      expect(price.range).to eq 7
     end
   end
 end
