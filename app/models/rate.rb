f3e812247b07f6036c33a3e260a170c5ec56a4c9
堀田太陽 <hottataiyo@hottataiyounoMacBook-Air.local>
2022-12-17 16:03:45 +0900

    fix: カスタムバリデーションを修正
    award == trueの時にバリデーションチェックを行う。falseの時は不要

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -49,15 +49,17 @@ class Rate < ApplicationRecord
   private
 
   def disallow_second_award_within_year
-    one_year_ago = Date.today - 1.year
-    num_award = Rate.
-      where(user_id: user_id).
-      where('created_at > ?', one_year_ago).
-      where(award: true)
+    if award == true
+      one_year_ago = Date.today - 1.year
+      num_award = Rate.
+        where(user_id: user_id).
+        where('created_at > ?', one_year_ago).
+        where(award: true)
 
-    if num_award.size >= 1
-      reenable_date = (num_award.first.created_at + 1.year).strftime("%Y年%m月%d日")
-      errors.add(:award, ": #{reenable_date}から可能です")
+      if num_award.size >= 1
+        reenable_date = (num_award.first.created_at + 1.year).strftime("%Y年%m月%d日")
+        errors.add(:award, ": #{reenable_date}から可能です")
+      end
     end
   end
 end
