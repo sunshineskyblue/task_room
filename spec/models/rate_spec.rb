8abf746650df33766c587334dc9400ad5632f2d1
堀田太陽 <hottataiyo@hottataiyounoMacBook-Air.local>
2022-12-18 13:05:14 +0900

    add: itを追加
    保証すべき動作として、ベスト評価が付帯されていない評価が過去のベスト評価の影響を受けずにDB
    に登録されること

--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -19,31 +19,36 @@ RSpec.describe Rate, type: :model do
   end
 
   describe '#disallow_second_award_within_year' do
-    let(:impressed_user) { create(:user) }
+    let(:user) { create(:user) }
     let(:today) { Date.today }
-    let(:next_award_rate) { build(:rate, user: impressed_user, award: true, created_at: today) }
+    let(:next_award_rate) { build(:rate, user: user, award: true, created_at: today) }
 
-    # 例) 直近2022年1月1日（ベスト評価日）の場合、2023年1月1日以降 => valid
+    # 例) 2022年1月1日（ベスト評価日）の場合 => 2023年1月1日以降 、評価再開
 
     context '本日が直近のベスト評価日から数えて1年目以降の場合' do
       let(:one_year_ago) { today - 1.year }
-      let!(:award_rate_one_year_ago) do
-        create(:rate, user: impressed_user, award: true, created_at: one_year_ago)
+      let!(:past_award_rate) do
+        create(:rate, user: user, award: true, created_at: one_year_ago)
       end
 
-      it '次の評価は許可されること' do
+      it '次のベスト評価は許可されること' do
         expect(next_award_rate).to be_valid
       end
     end
 
     context '本日が直近のベスト評価日から数えて1年未満の場合' do
       let(:less_than_one_year) { today - 1.year + 1.day }
-      let!(:award_rate_within_one_year) do
-        create(:rate, user: impressed_user, award: true, created_at: less_than_one_year)
+      let!(:past_award_rate) do
+        create(:rate, user: user, award: true, created_at: less_than_one_year)
+      end
+      let(:none_award_rate) { build(:rate, user: user, award: false, created_at: today) }
+
+      it '次のベスト評価は許可されないこと' do
+        expect(next_award_rate).to be_invalid
       end
 
-      it '次の評価は許可されないこと' do
-        expect(next_award_rate).to be_invalid
+      it 'ベスト評価を付けていない評価については、許可されること' do
+        expect(none_award_rate).to be_valid
       end
     end
   end
