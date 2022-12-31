8995b0097826d21f6c5939626b2dd4ea65dbd133
堀田太陽 <hottataiyo@hottataiyounoMacBook-Air.local>
2022-12-31 09:57:21 +0900

    add: roomsテーブルに人数カラムを追加
    人数の上限を設定できるようにする

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_26_034023) do
+ActiveRecord::Schema.define(version: 2022_12_31_005217) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 2022_12_26_034023) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "number"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
